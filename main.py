a08833a8dd70cd4591c82c96269373586565ef87
uklabelstore-stack <uklabelstore@gmail.com>
2026-09-15 17:58:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import subprocess
 import json
 
 app = Flask(__name__)
+CORS(app)  # Yeh browser blocking (CORS) ko khatam karega
 
 @app.route('/download', methods=['POST'])
 def download():
